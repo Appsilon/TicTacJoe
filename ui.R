6d0ec81aaf876178b8628c1b0d182c0eaecd69cd
Jędrzej Świeżewski <jedrzej@ppsilon.com>
2021-02-26 12:26:27 +0100

    removing spurious comments

--- ui.R
+++ ui.R
@@ -38,32 +38,9 @@ semanticPage(
     settings = list(c("autofocus", "false"))
   ),
   
-  # modal(
-  #   id = "game_modal",
-  #   target = "start_game",
-  #   header = "Let's play a game!",
-  #   footer = div(action_button(input_id = "cancel_game", label = "End game")),
-  #   div(class = "ui grid centered three column",
-  #       action_button(input_id = "top_left", label = ""),
-  #       action_button(input_id = "top_middle", label = ""),
-  #       action_button(input_id = "top_right", label = ""),
-  #   ),
-  #   div(class = "ui grid centered three column",
-  #       action_button(input_id = "middle_left", label = ""),
-  #       action_button(input_id = "middle_middle", label = ""),
-  #       action_button(input_id = "middle_right", label = ""),
-  #   ),
-  #   div(class = "ui grid centered three column",
-  #       action_button(input_id = "bottom_left", label = ""),
-  #       action_button(input_id = "bottom_middle", label = ""),
-  #       action_button(input_id = "bottom_right", label = ""),
-  #   )
-  # ),
-  
   tags$br(),
   tags$br(),
     
-
   div(class = "ui grid centered",
       split_layout(
         plotOutput("move_prob_1"),
