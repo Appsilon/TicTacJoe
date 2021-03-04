c963ff90fead4626a43baea91e0258c6f7a01a67
Jędrzej Świeżewski <jedrzej@ppsilon.com>
2021-03-04 13:03:22 +0100

    Added intro text to UI.

--- ui.R
+++ ui.R
@@ -9,6 +9,24 @@ semanticPage(
   tags$br(),
   
   div(class = "ui grid centered",
+      p("Welcome to TicTacJoe, a Reinforcement Learning agent which you can train to master the game of Tic Tac Toe."),
+      p("You can play against TicTicJoe by pressing the 'Play a game' button below.
+        When it is your turn, click a tile to make the move. 
+        When TicTacJoe is moving you will see the likelihood of him choosing a certain tile displayed on the tiles.
+        Note that some tiles are equivalent (symmetric) and hence don't get their separate likelihood."),
+      p("Initially, TicTacJoe performs each move randomly (all likelihoods are equivalent in a given turn), so is a noob in the game.
+        However, you can let TicTacJoe become better at the game, by letting him play against himself multiple times (click the 'Let TicTacJoe train' button).
+        Observe, how he gradually becomes a Tic Tac Toe guru :)"),
+      p("The three graphs at the bottom visualize the evolution of the likelihoods of the first three possible moves TicTacJoe can make when he starts a game.
+        They start from being equal (33% each), and as the training progresses, freeze out to always choosing the optimal move."),
+      p("To find out more about how TicTacJoe's brain works, ", a(href = 'https://appsilon.com', "read the accompanying blogpost.", .noWS = "outside"), ". Note, that the TicTacJoe's brain has been written in pure R, with no external libraries used.") 
+  ),
+  
+  tags$br(),
+  tags$br(),
+  tags$br(),
+  
+  div(class = "ui grid centered",
       action_button(input_id = "play_game", label = "Play a game")
   ),
   
